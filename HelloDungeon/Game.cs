fa07fdd1d2b8d5446c491d3d4e37f1dbb140b058
Reginald Thompson <reginaldjthompsonjr23@gmail.com>
2023-09-11 17:00:08 -0500

    attempted to implement mock demonstration of structs

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -8,9 +8,40 @@ namespace HelloDungeon
 {
     class Game
     {
+        struct Monster
+        {
+        public string monster1name;
+        public float monster1Health;
+        public float monster1Damage;
+        public float monster1Defense;
+        public float monster1stamina;
+        }
+
+        string monster1name = "killmonger";
+        float monster1Health = 500f;
+        float monster1Damage = 26f;
+        float monster1Defense = .87f;
+        float monster1stamina = 1f;
+        
+
+        string monster2Name = "gatling Man";
+        float monster2Health = 75f;
+        float monster2Damage = 200f;
+        float monster2Defense = .99f;
+        float monster2stamina = 3f;
+
+        string monster3Name = "acidSpitter";
+        float monster3Health = 25f;
+        float monster3Damage = 50f;
+        float monster3Defense = .12f;
+        float monster3stamina = 1f;
+
+
+
+
         public void Run()
         {
-
+            Monster test.Name - "killmoner";
         }
     }
 }
